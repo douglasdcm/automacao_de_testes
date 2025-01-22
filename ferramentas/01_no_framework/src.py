042d3d3c317f660dfb7041d0e6f9184e2b88b676
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-01-22 00:08:12 -0300

    Organize the folders and add pyautogui

--- ferramentas/01_no_framework/src.py
+++
@@ -1,4 +0,0 @@
-def sum(x, y):
-    if x == 0 or y == 0:
-        return -1
-    return x + y
--- ferramentas/05_dogtail/guara/utils.py
+++
@@ -1,3 +0,0 @@
-def get_transaction_info(transaction):
-    module_name = ".".join(transaction.__module__.split(".")[-1:])
-    return f"{module_name}.{transaction.__name__}"
