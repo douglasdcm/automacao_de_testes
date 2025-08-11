c5204e6b4b8491a83715eba22835bc97820a9d30
Douglas Cardoso <29078346+douglasdcm@users.noreply.github.com>
2025-08-11 02:12:57 -0300

    Add Playwright complete code

--- aulas/test_transactions_v3_ptp_playwright.py
+++ aulas/test_transactions_v3_ptp_playwright.py
@@ -33,15 +33,48 @@ class TestTransactionsV3:
         self._app.at(setup.CloseApp)
 
     def test_aulas(self, app: Application):
-        with_user_data = {
-            "username": "myname",
-            "password": "mypassword",
-            "email": "any@any.com",
-            "bio": "This is my bio",
-            "country": "United Kingdom",
-            "age": 30,
-            "birthdate": "1993-01-01",  # Adjusted date format for Playwright
-        }
-        app.at(home.FillForm, **with_user_data).asserts(
-            it.IsEqualTo, "Form would be submitted here"
+        # with_user_data = {
+        #     "username": "myname",
+        #     "password": "mypassword",
+        #     "email": "any@any.com",
+        #     "bio": "This is my bio",
+        #     "country": "United Kingdom",
+        #     "age": 30,
+        #     "birthdate": "1993-01-01",  # Adjusted date format for Playwright
+        # }
+        # self._app.at(home.FillForm, **with_user_data).asserts(
+        #     it.IsEqualTo, "Form would be submitted here"
+        # )
+        # self._app.at(home.CheckBoxes)
+        self._app.at(home.PushButtons, prompt_text="This is a prompt")
+        self._app.at(home.InteractWithDynamicElements).asserts(
+            it.HasKeyValue, {"hidden_element": "This element was hidden and now is visible!"}
+        ).asserts(it.HasKeyValue, {"new_element": "New element added "}).asserts(
+            it.HasKeyValue, {"changed_text": "Text changed at"}
+        ).asserts(
+            it.HasKeyValue,
+            {"toggled_class": "This element will change color when class is toggled"},
+        ).asserts(
+            it.HasKeyValue, {"ajax_content": "AJAX content loaded at"}
         )
+        self._app.at(
+            home.SelectDropdowns,
+            simple_option="Option 1",
+            multi_options=["Green", "Yellow"],
+            dynamic_option="Dynamic Option 2",
+        )
+        result = self._app.at(home.InteractWithTable).result
+        assert result["new_row_text"] == "New User 4"
+        assert result["original_number_of_rows"] == 3
+        assert result["new_number_of_rows"] == 4
+        assert result["number_of_rows_after_delete"] == 3
+        self._app.at(home.ShowPopover).asserts(it.Contains, "This is a tooltip")
+        self._app.at(
+            home.UploadFiles,
+            file_path="/home/douglas/repo/automacao_de_testes/aulas/docs/email.txt",
+            multi_file_path=[
+                "/home/douglas/repo/automacao_de_testes/aulas/docs/email.txt",
+                "/home/douglas/repo/automacao_de_testes/aulas/docs/UTF8demo.txt",
+            ],
+        )
+        self._app.at(home.InteractWithIFrame).asserts(it.IsEqualTo, "IFrame Content Loaded")
--- aulas/wrappers/playwright_wrapper.py
+++ aulas/wrappers/playwright_wrapper.py
@@ -1,25 +1,50 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 from wrappers.base_wrapper import BaseWrapper
 
 
 class PlaywrightWrapper(BaseWrapper):
     def __init__(self, driver: Page):
         self._driver: Page = driver
+        self._browser = self._driver
         self._dialog_message = ""
 
     def _dialog_handler(self, dialog):
         dialog.accept()
         self._dialog_message = dialog.message
 
+    def _dialog_handler_send_message(self, dialog):
+        dialog.send_text_to_alert("This is a prompt")
+        dialog.accept()
+        self._dialog_message = dialog.message
+
+    def dismiss_alert(self):
+        pass
+
+    def store_original_tab(self):
+        self._current_tab = self._driver.context.pages[0]
+
+    def close_current_tab(self):
+        self._driver.context.pages[-1].close()
+
+    def restore_original_tab(self):
+        self._current_tab.bring_to_front()
+
+    def send_text_to_alert(self, text):
+        self._driver.once("dialog", self._dialog_handler_send_message)
+
     def click(self, locator):
         # Register the dialog handler to capture alert messages
         self._driver.once("dialog", self._dialog_handler)
-        self._driver.locator(f"id={locator}").click()
+        self._driver.locator(f"id={locator}").click(force=True)
 
-    def send_text(self, locator, text):
+    def send_text(self, locator, text, clear=True):
         if not isinstance(text, str):
             text = str(text)
-        self._driver.locator(f"id={locator}").fill(text)
+        element = self._driver.locator(f"id={locator}")
+        if element.get_attribute("type") == "file":
+            element.set_input_files(text)
+        else:
+            element.fill(text)
 
     def select(self, locator, value):
         self._driver.locator(f"id={locator}").select_option(value)
@@ -27,8 +52,37 @@ class PlaywrightWrapper(BaseWrapper):
     def goto(self, url):
         self._driver.goto(url=url)
 
+    def get_text(self, locator):
+        text = self._driver.locator(f"id={locator}").text_content()
+        if not text:
+            text = self._driver.locator(f"id={locator}").input_value()
+        return text
+
+    def get_text_by_css(self, locator):
+        self._driver.locator(locator).wait_for(state="visible")
+        return self._driver.locator(locator).text_content()
+
+    def wait_for_text(self, locator, text):
+        expect(self._driver.locator(f"id={locator}")).to_contain_text(text)
+
     def get_dialog_text(self):
         return self._dialog_message
 
+    def get_number_of_rows(self, locator):
+        # Getting all rows from all tables, because when getting them by a specific
+        # table is not working properly
+        return self._driver.locator("tr").count() - 1  # Exclude header row
+
+    def hover_over_by_class_name(self, locator):
+        element = self._driver.locator(f".{locator}")
+        element.hover()
+        return element.text_content()
+
+    def switch_to_frame(self, locator):
+        frame = self._driver.frame_locator(f"iframe#{locator}")
+        if not frame:
+            raise ValueError(f"Frame with locator '{locator}' not found")
+        self._driver = frame
+
     def quit(self):
-        self._driver.close()
+        self._browser.close()
